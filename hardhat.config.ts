ca047e8835e9be8eb1195b065ff17109b92c77fa
Guy Birchal Robbe - Mac <gbrobbe@gmail.com>
2023-06-19 09:59:28 -0300

    Basic configuration + Add FundMe & PriceConverter contracts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -10,7 +10,32 @@ const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY! || "key"
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY! || "key"
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  solidity: "0.8.7",
+  defaultNetwork: "hardhat",
+  networks: {
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 11155111,
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      chainId: 31337,
+    },
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: false,
+    outputFile: "gas-report.txt",
+    noColors: true,
+    currency: "USD",
+    coinmarketcap: COINMARKETCAP_API_KEY,
+    token: "MATIC",
+  },
 }
 
 export default config
